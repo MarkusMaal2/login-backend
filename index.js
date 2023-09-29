9f8fcec5b83b82d3b03c3e74f25275842595e2e7
MarkusMaal2 <markus.maal@voco.ee>
2023-09-29 13:41:43 +0300

    Log in functionality with express-session

--- index.js
+++ index.js
@@ -1,35 +1,74 @@
 const express = require('express');
 const cors = require('cors');
 const { createHash } = require('crypto');
+const session = require("express-session");
 const app = express();
 
 app.use(cors());        // Avoid CORS errors in browsers
 app.use(express.json()) // Populate req.body
 
-const widgets = [
-    { id: 1, name: "Cizzbor", price: 29.99 },
-    { id: 2, name: "Woowo", price: 26.99 },
-    { id: 3, name: "Crazlinger", price: 59.99 },
-]
+let validSessions = []
 
 const salt = "#ALLLL24"
 
 let users = [
 ]
 
+app.use(session({
+    secret: 'GJeoeJJASwww',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false, maxAge: 60 * 30 * 1000 } // session limited to 30 minutes
+}))
+
 app.get('/users', (req, res) => {
-    res.send(users);
+    let returnId = -1;
+    if (req.body.password) {
+        let passWord = req.body.password;
+        users.forEach((user) =>  {
+            let userName = user.name;
+            let hash = user.hash;
+            if (createHash('sha256').update(userName  + salt + passWord).digest('hex') === hash) {
+                returnId = user.id;
+            }
+        })
+    }
+    if (returnId > 0) {
+        if (!validSessions.includes(req.sessionID)) {
+            validSessions.push(req.sessionID);
+            req.session.user = users[req.params.id - 1];
+            res.status(200).send({...users[returnId - 1], token: req.sessionID});
+        } else {
+            res.status(400).send({error: "The session is already active. Please log out to log in."})
+        }
+    } else {
+        res.status(401).send({error: "Invalid credentials"})
+    }
 })
 
 app.get('/users/:id', (req, res) => {
     if (typeof users[req.params.id - 1] === 'undefined') {
         return res.status(404).send({ error: "User not found" })
     }
-    res.send(users[req.params.id - 1])
+    if (req.body.password) {
+        let userName = users[req.params.id - 1].name;
+        let passWord = req.body.password;
+        let hash = users[req.params.id - 1].hash;
+        if (!validSessions.includes(req.sessionID)) {
+            validSessions.push(req.sessionID);
+            req.session.user = users[req.params.id - 1];
+            if (createHash('sha256').update(userName  + salt + passWord).digest('hex') !== hash) {
+                res.status(401).send({error: "Invalid credentials"})
+                return;
+            }
+        }
+        res.send({...users[req.params.id - 1], token: req.sessionID})
+    } else {
+        res.status(401).send({error: "Missing credentials"})
+    }
 })
 
 app.post('/users', (req, res) => {
-    console.log(req.body.length);
     if (!req.body.name || !req.body.password) {
         return res.status(400).send({ error: 'One or all params are missing' })
     }
@@ -45,9 +84,44 @@ app.delete('/users/:id', (req, res) => {
     if (typeof users[req.params.id - 1] === 'undefined') {
         return res.status(404).send({ error: "User not found" })
     }
-    users.splice(req.params.id-1, 1);
-    res.status(204).send({error: "No content"});
+    let returnId = -1;
+    if (req.body.password) {
+        let passWord = req.body.password;
+        users.forEach((user) =>  {
+            let userName = user.name;
+            let hash = user.hash;
+            if (createHash('sha256').update(userName  + salt + passWord).digest('hex') === hash) {
+                returnId = user.id;
+            }
+        })
+    }
+    if (returnId === req.params.id) {
+        users.splice(req.params.id-1, 1);
+        res.status(204).end();
+    } else {
+        res.status(401).send({error: "Invalid credentials"})
+    }
 })
+
+app.get("/logout", (req, res) => {
+    if (validSessions.includes(req.sessionID)) {
+        let erasedToken = "";
+        let erasedUserObject = {};
+        for (let i = 0; i < validSessions.length; i++) {
+            if (validSessions[i] === req.sessionID) {
+                erasedToken = validSessions[i];
+                erasedUserObject = req.session.user;
+                validSessions.splice(i, 1)
+            }
+        }
+        res.status(200).send({...erasedUserObject, token: erasedToken})
+        req.session.destroy();
+    } else {
+        res.status(400).send({error: "Invalid session token"})
+    }
+})
+
+
 
 app.listen(8080, () => {
     console.log(`Testing API up at: http://localhost:8080`)
