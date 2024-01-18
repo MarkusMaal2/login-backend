46584298ce9f339df0cd9d98ee7a77c214a7fb53
MarkusMaal2 <markus.maal@voco.ee>
2024-01-18 14:42:51 +0200

    Enable rolling session token

--- index.js
+++ index.js
@@ -427,7 +427,7 @@ const main = () => {
         secret: secret,
         resave: false,
         saveUninitialized: true,
-        rolling: false,
+        rolling: true,
         cookie: {
             secure: true,
             httpOnly: false,
