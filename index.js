cb1337d471ec88bf87777b77197df4c632127d45
MarkusMaal2 <markus.maal@voco.ee>
2024-01-18 18:39:12 +0200

    CORS allow origin to https://login-frontend-jet.vercel.app

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ connection.connect((err) => {
 });
 
 app.use(cors({
-    origin: ["http://localhost:3000", "https://localhost:3000", "https://localhost:3001", "http://localhost:3001", "http://maalmarkus.ikt.khk.ee", "https://maalmarkus.ikt.khk.ee", "https://login-frontend-klnmxcoj3-markusmaal2s-projects.vercel.app"],
+    origin: ["http://localhost:3000", "https://localhost:3000", "https://localhost:3001", "http://localhost:3001", "http://maalmarkus.ikt.khk.ee", "https://maalmarkus.ikt.khk.ee", "https://login-frontend-klnmxcoj3-markusmaal2s-projects.vercel.app", "https://login-frontend-jet.vercel.app"],
     credentials: true
 }));        // Avoid CORS errors in browsers
 app.use(express.json()) // Populate req.body
